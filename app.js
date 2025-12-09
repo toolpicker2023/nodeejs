e958ffc73c936d7c2251eca75512a1795d412a23
toolpicker2023 <aitools2024@protonmail.com>
2025-12-09 14:17:19 +0200

    changed port back to 3000

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const bcrypt = require("bcrypt");
 const { render } = require("ejs");
